9acb645270b5b9e9f4c7e47c949c3aaf8ff09bc7
Chazaray Robinson <chaarayrobinson@Chazarays-MacBook-Pro.local>
2019-10-22 16:46:08 -0400

    WIP

--- split/src/components/Bill.js
+++ split/src/components/Bill.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
-import Split from './split';
+// import Split from './split';
 // import Input from 
 
 const useStyles = makeStyles(theme => ({
@@ -57,7 +57,7 @@ export default function Bill() {
 
 
 
-                        <Split />
+                        {/* <Split /> */}
 
 
 
