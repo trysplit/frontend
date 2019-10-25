33df5bd3bda98d37f81f2b14dd009f29862bdeac
Bayron Puac <bayronpuac@gmail.com>
2019-10-25 01:25:23 -0500

    Added style to the Home component.

--- split/src/components/Home.js
+++ split/src/components/Home.js
@@ -9,21 +9,32 @@ const Home = () => {
        display: flex;
        flex-direction: column;
        justify-content: center;
-       align-items: center;
-       /* height: 100vh; */
-       img {
-           width:25rem;
-       }
-       p {
-        font-style: italic;
-        fomt-size: 40px;
-        margin: 0;
+       align-items: center;padding: 30rem 0; 
+       p{
+        width: 100%;
+        text-align: center;
+        margin-bottom: 5%;
+        font-size: 4.5rem;
+        overflow: hidden;
+        background: linear-gradient(90deg, darkolivegreen, goldenrod, forestgreen);
+        background-repeat: repeat;
+        background-size: 80%;
+        animation: animate 5s linear infinite;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: rgba(255, 255, 255, 0);
        }
        button {
-           background: #6E588A;
-           border-style: none;
-           width: 80px;
-           margin: 1rem;
+        width: 8rem;
+        background: #6E588A;
+        background-color: darkolivegreen;
+        margin: 0 10px 20px;
+        cursor: pointer;
+        width: 200px;
+        height: 40px;
+        font-size: 2rem;
+        border-radius: 10px;
+        color: white;
+        font-family: 'Noticia Text', serif;
        }
     `;
 
@@ -33,9 +44,9 @@ const Home = () => {
 
 
             <p>Returning user? click here to log in</p>
-            <Link to='/login'><Button>Log in</Button></Link>
+            <Link className="Link" to='/login'><Button>Log in</Button></Link>
             <p>New user? click here to register</p>
-            <Link to='/register'><Button>Register</Button></Link>
+            <Link className="Link"  to='/register'><Button>Register</Button></Link>
         </HomeCont>
     )
 }
